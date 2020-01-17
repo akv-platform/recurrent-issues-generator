2755b9169e6569492b7054c7aa80760fb2e9942e
Vladimir Safonkin <vsafonkin@gmail.com>
2020-01-17 21:28:54 +0300

    Test

--- test.ps1
+++ test.ps1
@@ -1,3 +1,9 @@
 Write-Host "Milestone!!!"
 $eventPayload = Get-Content $env:GITHUB_EVENT_PATH | ConvertFrom-Json
-Write-Host $eventPayload.milestone
+$milestoneTitle = $eventPayload.milestone
+$milestoneDescription = $eventPayload.description
+$milestoneDate = $eventPayload.created_at
+
+Write-Host"Title: $milestoneTitle"
+Write-Host"Description: $milestoneTitle"
+Write-Host"Date: $milestoneTitle"
