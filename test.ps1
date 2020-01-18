934abd9d0b4f6d4de4852fb9fd8c6145fbb30e08
Vladimir Safonkin <vsafonkin@gmail.com>
2020-01-18 21:52:25 +0300

    Test

--- test.ps1
+++ test.ps1
@@ -2,11 +2,12 @@ Import-Module (Join-Path $PSScriptRoot -ChildPath "GithubGraphQLApi.psm1")
 
 
 $eventPayload = Get-Content $env:GITHUB_EVENT_PATH | ConvertFrom-Json
-Write-Host $eventPayload.repository.owner
+# Write-Host $eventPayload.repository.owner
 $milestoneTitle = $eventPayload.milestone.title
 $milestoneDescription = $eventPayload.milestone.description
 $milestoneId = $eventPayload.milestone.node_id
 $repositoryName = $eventPayload.repository.name
+$repositoryOwner = $eventPayload.repository.owner.login
 $repositoryId = $eventPayload.repository.node_id
 
 # if ($milestoneTitle -NotMatch "\d{4} Week \d") {
@@ -25,7 +26,7 @@ $githubGraphQlApi = Get-GithubGraphQlApi -BearerToken $env:GITHUB_TOKEN
 #     Write-Host "Issue `"$title`" is created"
 # }
 
-$labels = $githubGraphQlApi.GetRepoLabels($repositoryName, "test-github-actions")
+$labels = $githubGraphQlApi.GetRepoLabels($repositoryOwner, $repositoryName)
 foreach ($label in $labels) {
     Write-Host $label
 }
