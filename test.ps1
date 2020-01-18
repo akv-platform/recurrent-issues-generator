e1284750b676af938346c72ce6d6f59e55c2475e
Vladimir Safonkin <vsafonkin@gmail.com>
2020-01-18 18:40:44 +0300

    Test

--- test.ps1
+++ test.ps1
@@ -1,18 +1,9 @@
-Write-Host "Milestone!!!"
 $eventPayload = Get-Content $env:GITHUB_EVENT_PATH | ConvertFrom-Json
 $milestoneTitle = $eventPayload.milestone.title
 $milestoneDescription = $eventPayload.milestone.description
-$milestoneDate = $eventPayload.milestone.created_at
-$milestoneNumber = $eventPayload.milestone.number
+$milestoneId = $eventPayload.milestone.node_id
 $repositoryId = $eventPayload.repository.node_id
-Write-Host $eventPayload
-Write-Host "------------"
-Write-Host "Title: $milestoneTitle"
-Write-Host "Description: $milestoneDescription"
-Write-Host "Date: $milestoneDate"
-Write-Host "Number: $milestoneNumber"
-Write-Host "Repository Id: $repositoryId"
-Write-Host "------------"
+
 
 
 # if ($milestoneTitle -NotMatch "\d\d\d\d Week \d") {
@@ -22,19 +13,24 @@ Write-Host "------------"
 Write-Host "Create issues..."
 
 $githubGraphQLApi = "https://api.github.com/graphql"
-$query = "mutation {createIssue(input:{title:`"hello github`", repositoryId:`"$repositoryID`"})
-               {issue {title}}
-                    }"
-$bodyGraphQl = @{
+$query = "mutation {
+            createIssue(input:{
+                            title:`"hello github`", 
+                            repositoryId:`"$repositoryID`",
+                            milestoneId:`"$milestoneId`"
+                        })
+            {issue {title}
+            }
+         }"
+
+$requestGraphQl = @{
     query = $query
 } | ConvertTo-Json
-
-Write-Host $bodyGraphQl
 
 $params = @{
     Uri = $githubGraphQLApi
     Method = "POST"
-    Body = $bodyGraphQl
+    Body = $requestGraphQl
     Headers = @{
         Authorization = "Bearer $env:GITHUB_TOKEN"
     }
